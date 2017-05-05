1938c31694311294787ca16ee0bd785faa9b0b8d
borcha <borisbokan@gmail.com>
2017-05-05 15:06:56 +0200

    Pocetak zadatak 12 sa termina 13

---
+++ app/src/main/java/rs/aleph/android/example12/activities/ThirdActivity.java
@@ -0,0 +1,50 @@
+package rs.aleph.android.example12.activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by androiddevelopment on 29.4.17..
+ */
+
+public class ThirdActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+}
